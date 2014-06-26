c1b9484d3186fa0b7a00cf55190d92f0bc99c45b
Michiel Stigter <michielstigter@hotmail.com>
2014-06-26 13:10:32 +0100

    completed the challenges

--- directory.rb
+++ directory.rb
@@ -6,19 +6,19 @@ end
 def display(students)
 	cohort = students.map { |student| student[:cohort] }.uniq 
 	cohort.each do |month|
-		puts "The students in the #{month.to_s} cohort are:" 
-			students.each do |student|
-				puts "#{student[:name]}, #{student[:age]}, #{student[:cohort]}" if student[:cohort] == month
-			end
+		puts "The students in the #{month} cohort are:" 
+		students.each do |student|
+			puts "#{student[:name]}, #{student[:age]}, #{student[:cohort]}" 
+		end
 	end
 end
 
 def print_footer(names)
 	if names.length > 1
 		print "Overall, we have #{names.length} great students\n" 
-		elsif  
-			print "Now we have #{names.length} student\n"
-		end
+	elsif  
+		print "Now we have #{names.length} student\n"
+	end
 end
 
 def input_students
@@ -36,7 +36,7 @@ def input_students
 			age = gets.chomp
 		end	
 
-	print "Please enter a number between 1-12 for the month of your cohort (1-January, etc)\n"
+		print "Please enter a number between 1-12 for the month of your cohort (1-January, etc)\n"
 	# cohortMonth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 	cohort = gets.chomp
 	# cohort = cohortMonth[cohort.to_i - 1]
@@ -47,25 +47,33 @@ def input_students
 
 	cohort = (Time.new(Time.now.year, cohort.to_i).strftime "%B").to_sym
 
-	puts "Your input is #{name}, #{age}, #{cohort}, are you sure?"
+	puts "Your input is #{name}, #{age}, #{cohort}, are you sure? Type y for yes and n for no"
 	confirmation = gets.chomp
+	while (confirmation != "y" && confirmation != "n") 
+		puts "Please enter y or n"
+		confirmation = gets.chomp
+	end
 	if confirmation == "y"
 
-		#add the student hash to the array
-		students << {:name => name, :age => age, :cohort => cohort}
-		if students.length > 1
-		print "Now we have #{students.length} students\n" 
-		elsif  
-			print "Now we have #{students.length} student\n"
-		end
-		#gets another name from the user
-		print "Please enter your name OR press return to exit\n"
-		name = gets.chomp
-	elsif confirmation == "n"
-		puts "Please re-enter your name"
-		name = gets.chomp
+				#add the student hash to the array
+				students << {:name => name, :age => age, :cohort => cohort}
+				if students.length > 1
+					print "Now we have #{students.length} students\n" 
+				elsif  
+					print "Now we have #{students.length} student\n"
+				end
+				#gets another name from the user
+				print "Please enter your name OR press return to exit\n"
+				name = gets.chomp
+
+			elsif confirmation == "n"
+				puts "Please re-enter your name"
+				name = gets.chomp
+
+
 	end
-end
+
+	end
 	#return the array of students
 	students
 end
